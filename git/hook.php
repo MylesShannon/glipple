656948a6f2d4276d0834db1de3d6794cd36a3f04
Myles Shannon <mylios101@gmail.com>
2015-02-04 21:08:40 -0500

    hook alt

--- git/hook.php
+++ git/hook.php
@@ -2,4 +2,10 @@
 echo "hook.php";
 
 exec("cd /var/www/html && git pull");
+
+/*
+if ( $_POST['payload'] ) {
+  shell_exec( 'cd /srv/www/git-repo/ && git reset --hard HEAD && git pull' );
+}
+*/
 ?>
