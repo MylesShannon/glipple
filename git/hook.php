69670e45c74bae3b4fc1267e3788f8804a87a348
Myles Shannon <mylios101@gmail.com>
2015-02-04 21:19:05 -0500

    hook debugging

--- git/hook.php
+++ git/hook.php
@@ -1,4 +1,10 @@
 <?php
+#!/usr/bin/php
+ini_set("display_errors", 1);
+ini_set("track_errors", 1);
+ini_set("html_errors", 1);
+error_reporting(E_ALL);
+
 echo "hook.php";
 
 exec("cd /var/www/html && git pull");
