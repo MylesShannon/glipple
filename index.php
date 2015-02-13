1a7602073e5abf41cca0e0b2a1d4aedff56e1b05
mshem <mshem@brandeis.edu>
2015-02-12 21:43:47 -0500

    re added stickyfooter

--- index.php
+++ index.php
@@ -34,6 +34,7 @@
     <!-- CSS -->
     <link rel="stylesheet" href="<?php echo URL; ?>public/css/reset.css" />
     <link rel="stylesheet" href="<?php echo URL; ?>public/css/style.css" />
+	<link rel="stylesheet" href="<?php echo URL; ?>public/css/stickyfooter.css" />
 	
 	<!-- google analytics 
 	<script type="text/javascript" src="<?php echo URL; ?>public/js/google.js"></script>
