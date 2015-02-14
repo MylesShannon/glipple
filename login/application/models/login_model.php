24c0ae028f779b80a24c19b56e6312cc08f85983
Myles Shannon <mylios101@gmail.com>
2015-02-14 00:10:42 -0500

    change passcode check

--- login/application/models/login_model.php
+++ login/application/models/login_model.php
@@ -35,7 +35,7 @@ class LoginModel
             return false;
         }
 		
-		if (!isset($_POST['passcode']) OR empty($_POST['passcode']) OR $_POST['passcode'] != PASSCODE) {
+		if ($_POST['passcode'] != '12345') {
             $_SESSION["feedback_negative"][] = "Passcode missing";
             return false;
         }
