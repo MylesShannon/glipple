0b388d8650ced292ecb7f91a646e4fe891604ad8
mshem <mshem@brandeis.edu>
2015-02-13 20:02:47 -0500

    attempt to fix downloading
    changed one line according to stack overflow

--- dl.php
+++ dl.php
@@ -16,7 +16,7 @@ header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 header('Cache-Control: public');
 header('Content-Description: File Transfer');
 header('Content-Type: application/force-download');
-header('Content-Disposition: attachment; filename='.$file);
+header('Content-Disposition: attachment; filename=$file");
 header('Content-Transfer-Encoding: binary');
 header('Content-Length: '.$len);
 readfile($file);
