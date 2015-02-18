2e09f8caf95fb0586a81100e90e085ba5f0c9c47
Michael Shemesh <mshem@brandeis.edu>
2015-02-17 23:54:01 -0500

    added needed space

--- login/application/views/upload/upld.php
+++ login/application/views/upload/upld.php
@@ -28,9 +28,10 @@ echo "File basename:".basename( $_FILES["file"]["name"]);
 
 
  $uploadOk = 1;
- if (!isset($_FILES["file"]){
+ if (!isset($_FILES["file"])
+ {
 	$uploadok =0;
-}
+	}	
 $target_dir = $userDir ."/". basename( $_FILES["file"]["name"]);
 
 $songFileType = pathinfo($target_dir,PATHINFO_EXTENSION);
