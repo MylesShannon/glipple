507f4f0131c9fe4958b2d8993c0076b513676aba
Myles Shannon <mylios101@gmail.com>
2015-02-17 22:52:59 -0500

    re-try ON DUPLICATE

--- login/application/views/login/upload_bio.php
+++ login/application/views/login/upload_bio.php
@@ -14,10 +14,11 @@ $userID = Session::get('user_id');
 $bio = $_POST['bandBio'];
 
 // mysql_query("UPDATE profiles SET band_bio = '$bio' WHERE user_id = $userID") or die(mysql_error());
-// mysql_query("INSERT INTO profiles (id, user_id, band_image, band_bio, timestamp) VALUES (NULL, '$userID', NULL, '$bio', NULL) ON DUPLICATE KEY UPDATE user_id = '$userID', band_bio = '$bio'") or die(mysql_error());
+mysql_query("INSERT INTO $table (id, user_id, band_image, band_bio, timestamp) VALUES (NULL, '$userID', NULL, '$bio', NULL) ON DUPLICATE UPDATE user_id = '$userID', band_bio = '$bio'") or die(mysql_error());
 
-$result = mysql_query("SELECT * FROM '$table' WHERE user_id = '$userID' ");
+// $result = mysql_query("SELECT * FROM '$table' WHERE user_id = '$userID' ");
 
+/*
 if( mysql_num_rows($result) > 0) {
     mysql_query("UPDATE '$table' SET band_bio = '$bio' WHERE user_id = '$userID' ");
 }
@@ -25,6 +26,7 @@ else
 {
     mysql_query("INSERT INTO '$table' (id, user_id, band_image, band_bio, timestamp) VALUES(NULL, '$userID', NULL, '$bio', NULL) ");
 }
+*/
 
 mysql_close();
 ?>
