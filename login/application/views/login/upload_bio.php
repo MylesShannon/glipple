9a55feed6d2ba9859ee3993aff30af964d597fd8
Myles Shannon <mylios101@gmail.com>
2015-02-17 23:08:45 -0500

    back to if

--- login/application/views/login/upload_bio.php
+++ login/application/views/login/upload_bio.php
@@ -14,11 +14,10 @@ $userID = Session::get('user_id');
 $bio = $_POST['bandBio'];
 
 // mysql_query("UPDATE profiles SET band_bio = '$bio' WHERE user_id = $userID") or die(mysql_error());
-mysql_query("INSERT INTO $table (user_id, band_bio) VALUES ('$userID','$bio') ON DUPLICATE KEY UPDATE band_bio = '$bio'") or die(mysql_error());
+// mysql_query("INSERT INTO $table (user_id, band_bio) VALUES ('$userID','$bio') ON DUPLICATE KEY UPDATE band_bio = '$bio'") or die(mysql_error());
 
-// $result = mysql_query("SELECT * FROM '$table' WHERE user_id = '$userID' ");
+$result = mysql_query("SELECT user_id FROM '$table' WHERE user_id = '$userID' ");
 
-/*
 if( mysql_num_rows($result) > 0) {
     mysql_query("UPDATE '$table' SET band_bio = '$bio' WHERE user_id = '$userID' ");
 }
@@ -26,7 +25,6 @@ else
 {
     mysql_query("INSERT INTO '$table' (id, user_id, band_image, band_bio, timestamp) VALUES(NULL, '$userID', NULL, '$bio', NULL) ");
 }
-*/
 
 mysql_close();
 ?>
