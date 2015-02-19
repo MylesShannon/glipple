1f3a1947cb775b7c975a2999af84cdf9a44cba94
Myles Shannon <mylios101@gmail.com>
2015-02-19 03:32:05 -0500

    search back to home on blank

--- public/js/content.js
+++ public/js/content.js
@@ -86,7 +86,7 @@ function loadsearchresults(s)
 	xmlhttp.send();
 	*/
 	$.ajax({
-        url: "/views/search.php?s="+s,
+        if(s != NULL) {url: "/views/search.php?s="+s} else {url: "/views/index.php"},
         context: document.body,
         success: function(result) {
             $("#filler").html(result);
