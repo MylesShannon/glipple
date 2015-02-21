31cfe68cd595558e2db9567e606b0e23b94394ee
mshem <mshem@brandeis.edu>
2015-02-21 00:58:59 -0500

    proflie template!

---
+++ views/profile.php
@@ -0,0 +1,33 @@
+<table id="profile">
+<tr>
+
+
+<td >
+<?php 
+
+	$userid = trim($_GET["id"]);
+	$server = "localhost";
+	$user = "root";
+	$pass = "4DaL0v3AM0n3y";
+	$db = "login";
+	
+	
+	mysql_connect($server, $user, $pass) or die(mysql_error());
+	mysql_select_db($db) or die(mysql_error());
+
+	$result = mysql_query("SELECT band_bio FROM profiles WHERE user_id LIKE ".$userid) or die(mysql_error());  
+
+	echo $result;
+	
+
+?>
+</td>
+
+
+<td>
+
+</td>
+
+
+</tr>
+</table>
