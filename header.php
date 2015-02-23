6e93d7ebc7601dcb524412d0907d61ce0221bbbc
Myles Shannon <mylios101@gmail.com>
2015-02-22 19:34:41 -0500

    hard link datatables

--- header.php
+++ header.php
@@ -3,8 +3,8 @@ define('URL', 'http://glipple.com/');
 ?>
 	
 	<!-- load jQuery -->
-	<!-- <script type="text/javascript" src="<?php echo URL; ?>public/js/jquery-2.1.1.js"></script> -->
-	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="<?php echo URL; ?>public/js/jquery-2.1.1.js"></script>
+	<!-- <script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.min.js"></script> -->
 	
 	<!-- load dropzone css and js -->
 	<link rel="stylesheet" href="<?php echo URL; ?>public/css/dropzone.css" type="text/css" />
@@ -14,8 +14,10 @@ define('URL', 'http://glipple.com/');
 	<script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.jplayer.min.js"></script>
 	<link type="text/css" href="<?php echo URL; ?>public/js/jPlayer/skins/blue.monday/jplayer.blue.monday.css" rel="stylesheet" />
 	<script type="text/javascript" src="<?php echo URL; ?>public/js/initjplayer.js"></script>
-	<script type="text/javascript" src="//cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
-	<link rel="stylesheet" href="//cdn.datatables.net/1.10.4/css/jquery.dataTables.css" />
+	
+	<!-- dataTables -->
+	<script type="text/javascript" src="<?php echo URL; ?>public/js/jquery.dataTables.min.js"></script>
+	<link rel="stylesheet" href="<?php echo URL; ?>public/css/jquery.dataTables.css" />
 
 	<!-- using this to make on the fly editable fields
 	<script src="<?php echo URL; ?>public/js/prototype.js" type="text/javascript"></script>
