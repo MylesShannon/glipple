4733b45d7e69d8867b8aed4845ed153697991801
Myles Shannon <mylios101@gmail.com>
2015-02-23 20:15:36 -0500

    type needs to be 'submit'

--- login/application/views/music/index.php
+++ login/application/views/music/index.php
@@ -56,7 +56,7 @@ error_reporting(E_ALL ^ E_DEPRECATED);
 		echo "<td>".$row['album']."</td>";
 		echo "<td>".$row['artist']."</td>";
 		echo "<td>".$row['genre']."</td>";
-		echo "<td><button id='delete'>delete</button></td>";
+		echo "<td><button id='delete' type='submit'>delete</button></td>";
 		echo "</tr>";
 		$count++;
 	}
