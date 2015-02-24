2298bcd4206aea1bb626803b54ad6252dd25e36c
Myles Shannon <mylios101@gmail.com>
2015-02-23 19:53:52 -0500

    pass $id

--- login/application/views/music/delete.php
+++ login/application/views/music/delete.php
@@ -7,30 +7,30 @@ echo $_POST["del"];
 echo $id;
 
 if(isset($_POST['del'])) {
-	deleteSong();
+	deleteSong($id);
 } else {
 	echo "No submission!";
 }
 
 
-function deleteSong() {
-// connect to mysql
-$server = "localhost";
-$user = "root";
-$pass = "4DaL0v3AM0n3y";
-$db = "music";
-mysql_connect($server, $user, $pass) or die(mysql_error());
-mysql_select_db($db) or die(mysql_error());
+function deleteSong($id) {
+	// connect to mysql
+	$server = "localhost";
+	$user = "root";
+	$pass = "4DaL0v3AM0n3y";
+	$db = "music";
+	mysql_connect($server, $user, $pass) or die(mysql_error());
+	mysql_select_db($db) or die(mysql_error());
 
 
-// take passed info and commit it to db
-$result = mysql_query("DELETE FROM id3 WHERE id = '$id'") or die(mysql_error());
-  
-// delete file
-unlink("/media/music/".Session::get('user_id')."/".$id.".mp3");
+	// take passed info and commit it to db
+	$result = mysql_query("DELETE FROM id3 WHERE id = '$id'") or die(mysql_error());
+	  
+	// delete file
+	unlink("/media/music/".Session::get('user_id')."/".$id.".mp3");
 
-echo "deleted song '$id'";
-// close sql connection
-mysql_close();
+	echo "deleted song '$id'";
+	// close sql connection
+	mysql_close();
 }
 ?>
