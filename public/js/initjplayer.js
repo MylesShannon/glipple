3aad6dbf16ca4e51c83da554e81ef2e5e4334f49
Myles Shannon <mylios101@gmail.com>
2015-02-25 04:45:31 -0500

    different jPlayer javascripts

--- public/js/initjplayer.js
+++ public/js/initjplayer.js
@@ -38,6 +38,7 @@ $(document).ready(function(){
 */
 
 // Myles' player
+/*
 $(document).ready(function(){
 	var stream = {
 		m4a: "http://glipple.com:8000/radio"
@@ -65,7 +66,8 @@ $(document).ready(function(){
 		keyEnabled: true
 	});
 });
-		
+*/
+
 // tut's player 
 /*
 $(document).ready(function(){
@@ -89,3 +91,54 @@ toggleDuration: true
 });
 });
 */
+
+status.formatType = 'mp3'
+Browser canPlay('audio/mpeg')
+
+status.src = 'http://glipple.com/radio'
+
+status.media = {
+ title: Glipple Radio
+ mp3: http://glipple.com/radio
+};
+
+status.videoWidth = '0' | status.videoHeight = '0'
+status.width = '0px' | status.height = '0px'
+htmlElement.audio.canPlayType = function
+
+$('#jquery_jplayer_1').jPlayer({
+ swfPath: './js/jPlayer/jPlayer.swf',
+ solution: 'html, flash',
+ supplied: 'mp3',
+ preload: 'none',
+ volume: 0.8,
+ muted: false,
+ backgroundColor: '#000000',
+ cssSelectorAncestor: '#jp_container_1',
+ cssSelector: {
+  videoPlay: '.jp-video-play',
+  play: '.jp-play',
+  pause: '.jp-pause',
+  stop: '.jp-stop',
+  seekBar: '.jp-seek-bar',
+  playBar: '.jp-play-bar',
+  mute: '.jp-mute',
+  unmute: '.jp-unmute',
+  volumeBar: '.jp-volume-bar',
+  volumeBarValue: '.jp-volume-bar-value',
+  volumeMax: '.jp-volume-max',
+  playbackRateBar: '.jp-playback-rate-bar',
+  playbackRateBarValue: '.jp-playback-rate-bar-value',
+  currentTime: '.jp-current-time',
+  duration: '.jp-duration',
+  title: '.jp-title',
+  fullScreen: '.jp-full-screen',
+  restoreScreen: '.jp-restore-screen',
+  repeat: '.jp-repeat',
+  repeatOff: '.jp-repeat-off',
+  gui: '.jp-gui',
+  noSolution: '.jp-no-solution'
+ },
+ errorAlerts: false,
+ warningAlerts: false
+});
