2a9d6ec83b005072cf089ae995e3515c8fa0ed86
Myles Shannon <mylios101@gmail.com>
2015-02-25 02:40:53 -0500

    float dat input

--- login/application/views/login/register.php
+++ login/application/views/login/register.php
@@ -37,8 +37,8 @@
             <label for="login_input_passcode">Passcode</label>
 			<input id="login_input_passcode" class="passcode_input" type="password" name="passcode" required autocomplete="off" />
 			
-			<label for="login_input_tos">Agree to the <a href='<?php echo URL; ?>' >Terms of Service</a>
-			<input id="login_input_tos" class="tos_input" type="checkbox" name="checkbox" required /></label>
+			<label for="login_input_tos">Agree to the <a href='<?php echo URL; ?>' >Terms of Service</a></label>
+			<input style="float:left" id="login_input_tos" class="tos_input" type="checkbox" name="checkbox" required />
 			
 			<!-- show the captcha by calling the login/showCaptcha-method in the src attribute of the img tag -->
             <!-- to avoid weird with-slash-without-slash issues: simply always use the URL constant here -->
