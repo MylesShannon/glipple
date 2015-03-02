c7045ed4c80e4ed740a35c2eb1f7ada079d0826b
Myles Shannon <mylios101@gmail.com>
2015-03-02 18:44:40 -0500

    correct mike's name

--- views/contact.php
+++ views/contact.php
@@ -3,7 +3,7 @@
 <h3>The co-founders:</h3>
 <p class='email'><a href="mailto:amarx@glipple.com">Adam Marx - CEO</a></p>
 <p class='email'><a href="mailto:myles@glipple.com">Myles Shannon - CTO</a></p>
-<p class='email'><a href="mailto:mshem@glipple.com">Miachel Shemesh - CIO</a></p>
+<p class='email'><a href="mailto:mshem@glipple.com">Michael Shemesh - CIO</a></p>
 <p class='email'><a href="mailto:smarx@glipple.com">Shelly Marx - VP of Artist Relations</a></p>
 
 <br />
