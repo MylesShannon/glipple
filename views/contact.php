714e0287943a53b38cb266957d150e68b8be16b7
Myles Shannon <mylios101@gmail.com>
2015-03-02 18:42:19 -0500

    text indent for emails

--- views/contact.php
+++ views/contact.php
@@ -1,10 +1,10 @@
 <h1>Contact</h1>
 
 <h3>The co-founders:</h3>
-<p><a href="mailto:amarx@glipple.com">	Adam Marx - CEO</a></p>
-<p><a href="mailto:myles@glipple.com">	Myles Shannon - CTO</a></p>
-<p><a href="mailto:mshem@glipple.com">	Miachel Shemesh - CIO</a></p>
-<p><a href="mailto:smarx@glipple.com">	Shelly Marx - VP of Artist Relations</a></p>
+<p class='email'><a href="mailto:amarx@glipple.com">Adam Marx - CEO</a></p>
+<p class='email'><a href="mailto:myles@glipple.com">Myles Shannon - CTO</a></p>
+<p class='email'><a href="mailto:mshem@glipple.com">Miachel Shemesh - CIO</a></p>
+<p class='email'><a href="mailto:smarx@glipple.com">Shelly Marx - VP of Artist Relations</a></p>
 
 <br />
 <p>Send us an email!</p>
