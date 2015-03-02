5b59d862bce700a2a1b8af968db4d00f0942cb16
Myles Shannon <mylios101@gmail.com>
2015-03-02 16:35:58 -0500

    does it work in latest still?

--- views/index.php
+++ views/index.php
@@ -49,5 +49,21 @@ $(document).ready(function() {
 		"bLengthChange": false
 		
     } );
+	
+	//load profile on click
+	$(".profile").click(function (event) {
+		var pro = event.target.id;
+		alert(pro);
+		profile(pro);
+	});
+	function profile(pro) {
+		$.ajax({
+			url: "/views/profile.php?id="+pro,
+			context: document.body,
+			success: function(result) {
+				$("#filler").html(result);
+			}	
+		});
+	}
 } );
 </script>
