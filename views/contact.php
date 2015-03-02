a1f55db1273f9e5ed314882d516645aea948bc9b
Myles Shannon <mylios101@gmail.com>
2015-03-02 18:36:22 -0500

    contact style update

--- views/contact.php
+++ views/contact.php
@@ -1,11 +1,10 @@
 <h1>Contact</h1>
 
-<h2>The co-founders:</h2>
-<p>
-	<a href="mailto:amarx@glipple.com">Adam Marx - CEO</a>
-	<a href="mailto:myles@glipple.com">Myles Shannon - CTO</a>
-	<a href="mailto:mshem@glipple.com">Miachel Shemesh - CIO</a>
-	<a href="mailto:smarx@glipple.com">Shelly Marx - VP of Artist Relations</a>
-</p>
+<h3>The co-founders:</h3>
+<p><a href="mailto:amarx@glipple.com">Adam Marx - CEO</a></p>
+<p><a href="mailto:myles@glipple.com">Myles Shannon - CTO</a></p>
+<p><a href="mailto:mshem@glipple.com">Miachel Shemesh - CIO</a></p>
+<p><a href="mailto:smarx@glipple.com">Shelly Marx - VP of Artist Relations</a></p>
 
+<br />
 <p>Send us an email!</p>
