12e30a465b74769c0ef9d50e2ce257372b71d840
Myles Shannon <mylios101@gmail.com>
2015-03-02 15:57:49 -0500

    playing.php with profile and download

--- views/playing.php
+++ views/playing.php
@@ -1,4 +1,14 @@
 <?php
+error_reporting(E_ALL ^ E_DEPRECATED);
+include "../header.php";
+
+$server = "localhost";
+$user = "root";
+$pass = "4DaL0v3AM0n3y";
+$db = "music";
+mysql_connect($server, $user, $pass) or die(mysql_error());
+mysql_select_db($db) or die(mysql_error());
+
 $fp = fsockopen("localhost", "1234", $errno, $errstr); 
 
 if (!$fp) {
@@ -11,9 +21,12 @@ if (!$fp) {
 		$output .=fgets($fp); 
     }
 	
+	preg_match('/filename="\/media\/music\/\/(.*)"/', $output, $file);
+	preg_match('/filename="\/media\/music\/\/(.*)\/(.*)"/', $output, $id);
 	preg_match('/title="(.*)"/', $output, $title);
 	preg_match('/artist="(.*)"/', $output, $artist);
-	echo $title[1]." - ".$artist[1];
+	
+	echo "<a href='".URL."music/".$file[1]."' download='".$title[1]."'>".$title[1]."</a> - <a href='#profile' id='".$id[1]."' class='profile'>".$artist[1]."</a>";
 }
 
 fclose($fp);
