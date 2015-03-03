1c81b629922e22e21f7f92fbf5e8d4e5feb950bc
Myles Shannon <mylios101@gmail.com>
2015-03-03 15:04:53 -0500

    doubled extensions

--- views/index.php
+++ views/index.php
@@ -31,8 +31,9 @@
 	while ($row = mysql_fetch_array($result)) {
 		preg_match('/\/media\/(.*)/', $row['path'], $path);
 		preg_match('/\/media\/music\/.*\/.*\.(.*)/', $row['path'], $type);
+		$title = preg_replace("/[^a-zA-Z0-9 ]+/", "", $row['title']);
 		echo "<tr><td>".$count++."</td>";
-		echo "<td><a href='".URL.$path[1]."' download='".preg_replace("/[^a-zA-Z0-9 ]+/", "", $row['title']).$type[1]."'>".$row['title']."</a></td>";
+		echo "<td><a href='".URL.$path[1]."' download='".$title.".".$type[1]."'>".$row['title']."</a></td>";
 		echo "<td><a href='#profile' id='".$row['owner']."' class='profile'>".$row['artist']."</a></td>";
 		echo "<td>".$row['album']."</td>";
         echo "<td>".$row['genre']."</td></tr>";
