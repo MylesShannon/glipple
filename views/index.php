e13f31de0bcb1ef533b9511b628fffcbfed5c158
Myles Shannon <mylios101@gmail.com>
2015-03-03 14:35:57 -0500

    fix download urls

--- views/index.php
+++ views/index.php
@@ -27,10 +27,11 @@
 
 	$result = mysql_query("SELECT * FROM id3 ORDER BY timestamp DESC") or die(mysql_error()); 
 	$count = 1;
+	preg_match('/\/media\/(.*)"/', mysql_fetch_array($result)['path'], $path);
 
 	while ($row = mysql_fetch_array($result)) {
 		echo "<tr><td>".$count++."</td>";
-		echo "<td><a href='".URL."music/".$row['owner']."/".$row['id'].".mp3' download='".preg_replace("/[^a-zA-Z0-9 ]+/", "", $row['title']).".mp3'>".$row['title']."</a></td>";
+		echo "<td><a href='".URL.$path"' download='".preg_replace("/[^a-zA-Z0-9 ]+/", "", $row['title']).".mp3'>".$row['title']."</a></td>";
 		echo "<td><a href='#profile' id='".$row['owner']."' class='profile'>".$row['artist']."</a></td>";
 		echo "<td>".$row['album']."</td>";
         echo "<td>".$row['genre']."</td></tr>";
