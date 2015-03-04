a670504f5cd4fd947e5002e70c3e3de77cb2e7c0
Myles Shannon <mylios101@gmail.com>
2015-03-04 13:30:03 -0500

    add remaining table tags

--- login/application/views/music/index.php
+++ login/application/views/music/index.php
@@ -65,6 +65,8 @@ error_reporting(E_ALL ^ E_DEPRECATED);
 -		echo "<td>".$row['album']."</td>";
 -		echo "<td>".$row['artist']."</td>";
 -		echo "<td>".$row['genre']."</td>";
+		echo "<td><button id='delete' value='".$row['id']."' type='submit'>delete</button></td>";
+ 		echo "</tr>";
 		
 		$count++;
 	}
