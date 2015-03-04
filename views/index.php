ef19b88d0aaf3793a807dcc90580dbce755a4533
mshem <mshem@brandeis.edu>
2015-03-04 00:13:49 -0500

    update downloads 1

--- views/index.php
+++ views/index.php
@@ -33,7 +33,7 @@
 		preg_match('/\/media\/music\/.*\/.*\.(.*)/', $row['path'], $type);
 		$title = preg_replace("/[^a-zA-Z0-9 ]+/", "", $row['title']);
 		echo "<tr><td>".$count++."</td>";
-		echo "<td><a class='dl' href='".URL.$path[1]."' onclick='updatedl(".$row['artist'].", ".$row['title'].")'download='".$title.".".$type[1]."'>".$row['title']."</a></td>";
+		echo "<td><a class='dl' href='".URL.$path[1]."' onclick='updatedl(".$row['artist'].", ".$row['title'].")' download='".$title.".".$type[1]."'>".$row['title']."</a></td>";
 		echo "<td><a href='#profile' id='".$row['owner']."' class='profile'>".$row['artist']."</a></td>";
 		echo "<td>".$row['album']."</td>";
         echo "<td>".$row['genre']."</td></tr>";
