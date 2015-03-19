2e018b1c9cad7a540cb373627ea5d56f3224b4b3
mshem <mshem@brandeis.edu>
2015-03-18 21:52:04 -0400

    Revert "audio to audio stream"
    This reverts commit c200a15ede8573e98741159e1351304113181f28.

--- views/radio.php
+++ views/radio.php
@@ -1,6 +1,6 @@
 <div class='radio'>
   <div id="jquery_jplayer_1" class="jp-jplayer"></div>
-  <div id="jp_container_1" class="jp-audio-stream" role="application" aria-label="media player">
+  <div id="jp_container_1" class="jp-audio" role="application" aria-label="media player">
     <div class="jp-type-single">
       <div class="jp-gui jp-interface">
         <div class="jp-controls">
