e527a3d0092cc096e28c56d5f25f6a596ad93513
Myles Shannon <mylios101@gmail.com>
2015-03-27 18:39:31 -0400

    Revert "simplify radio tags and integrate header"
    This reverts commit 95199094d87a842047b0ce812e241fafbae23f60.

--- views/radio.php
+++ views/radio.php
@@ -1,7 +1,19 @@
 <div class='radio'>
 	<div id="jquery_jplayer_1" class="jp-jplayer"></div>
 	<div id="jp_container_1" class="jp-audio" role="application" aria-label="media player">
-				<button class="jp-play" role="button" tabindex="0">play</button>
+		<div class="jp-type-single">
+			<div class="jp-gui jp-interface">
+				<div class="jp-controls">
+					<button class="jp-play" role="button" tabindex="0">play</button>
+					<button class="jp-stop" role="button" tabindex="0">stop</button>
+				</div>
+				<!--
+				<div class="jp-progress">
+					<div class="jp-seek-bar">
+						<div class="jp-play-bar"></div>
+					</div>
+				</div>
+				-->
 				<div class="jp-volume-controls">
 					<button class="jp-mute" role="button" tabindex="0">mute</button>
 					<button class="jp-volume-max" role="button" tabindex="0">max volume</button>
@@ -11,7 +23,14 @@
 				</div>
 				<div class="jp-time-holder">
 					<div class="jp-current-time" role="timer" aria-label="time">&nbsp;</div>
+					<!--
+					<div class="jp-duration" role="timer" aria-label="duration">&nbsp;</div>
+					<div class="jp-toggles">
+						<button class="jp-repeat" role="button" tabindex="0">repeat</button>
+					</div>
+					-->
 				</div>
+			</div>
 			<div class="jp-details">
 				<div class="jp-title" aria-label="title" id="song_title">&nbsp;</div>
 			</div>
@@ -19,9 +38,10 @@
 				<span>Update Required</span>
 				To play the media you will need to either update your browser to a recent version or update your <a href="http://get.adobe.com/flashplayer/" target="_blank">Flash plugin</a>.
 			</div>
+		</div>
 	</div>
 </div>
   
-<script type="text/javascript" src="<?php echo URL; ?>public/js/initjplayer.js"></script>
-<!-- load Now Playing js -->
-<script type="text/javascript" src="<?php echo URL; ?>public/js/playing.js"></script>
+  <script type="text/javascript" src="<?php echo URL; ?>public/js/initjplayer.js"></script>
+  <!-- load Now Playing js -->
+  <script type="text/javascript" src="<?php echo URL; ?>public/js/playing.js"></script>
