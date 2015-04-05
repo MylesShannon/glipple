b27de7ece0bcb6da55783522dceac4c6fd30d0b9
Myles Shannon <mylios101@gmail.com>
2015-04-05 16:10:46 -0400

    ra muse test file

---
+++ views/ra.php
@@ -0,0 +1,32 @@
+<!-- BEGINS: AUTO-GENERATED MUSES RADIO PLAYER CODE -->
+<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="329" height="21" bgcolor="#FFFFFF">
+<param name="movie" value="muses.swf" />
+<param name="flashvars" value="url=http://glipple.com/radio&lang=auto&codec=mp3&volume=20&introurl=&autoplay=true&tracking=true&jsevents=false&buffering=5&skin=original&title=Glipple%20Radio" />
+<param name="wmode" value="window" />
+<param name="allowscriptaccess" value="always" />
+<param name="bgcolor" value="#FFFFFF" />
+<param name="scale" value="noscale" />
+<embed src="muses.swf" flashvars="url=http://glipple.com/radio&lang=auto&codec=mp3&volume=20&introurl=&autoplay=true&tracking=true&jsevents=false&buffering=5&skin=original&title=Glipple%20Radio" width="329" scale="noscale" height="21" wmode="window" bgcolor="#FFFFFF" allowscriptaccess="always" type="application/x-shockwave-flash" />
+</object>
+<!-- ENDS: AUTO-GENERATED MUSES RADIO PLAYER CODE -->
+
+<br>
+<br>
+
+<!-- BEGINS: AUTO-GENERATED MUSES RADIO PLAYER CODE -->
+<script type="text/javascript" src="https://hosted.muses.org/mrp.js"></script>
+<script type="text/javascript">
+MRP.insert({
+'url':'http://glipple.com/radio',
+'codec':'mp3',
+'volume':20,
+'autoplay':true,
+'buffering':5,
+'title':'Glipple Radio',
+'bgcolor':'#FFFFFF',
+'skin':'original',
+'width':329,
+'height':21
+});
+</script>
+<!-- ENDS: AUTO-GENERATED MUSES RADIO PLAYER CODE -->
