02ef3becc1fa90342b8f8ad82731308a253ad87f
Myles Shannon <mylios101@gmail.com>
2015-04-11 01:43:04 -0400

    add player art display

--- views/radio.php
+++ views/radio.php
@@ -7,13 +7,6 @@
 					<button class="jp-play" role="button" tabindex="0">play</button>
 					<button class="jp-stop" role="button" tabindex="0">stop</button>
 				</div>
-				<!--
-				<div class="jp-progress">
-					<div class="jp-seek-bar">
-						<div class="jp-play-bar"></div>
-					</div>
-				</div>
-				-->
 				<div class="jp-volume-controls">
 					<button class="jp-mute" role="button" tabindex="0">mute</button>
 					<button class="jp-volume-max" role="button" tabindex="0">max volume</button>
@@ -23,12 +16,9 @@
 				</div>
 				<div class="jp-time-holder">
 					<div class="jp-current-time" role="timer" aria-label="time">&nbsp;</div>
-					<!--
-					<div class="jp-duration" role="timer" aria-label="duration">&nbsp;</div>
-					<div class="jp-toggles">
-						<button class="jp-repeat" role="button" tabindex="0">repeat</button>
-					</div>
-					-->
+				</div>
+				<div class="jp-art">
+					<img src="<?php echo URL; ?>public/img/songLogoSmall.png" alt="Song Art" height="85" width="85">
 				</div>
 			</div>
 			<div class="jp-details">
