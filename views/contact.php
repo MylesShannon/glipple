6a831dcb7338adad63571ca6692674ca5400cece
Myles Shannon <mylios101@gmail.com>
2015-04-13 22:44:07 -0400

    clickable names, not titles

--- views/contact.php
+++ views/contact.php
@@ -1,8 +1,8 @@
 <h3>The cofounders:</h3>
-<p class='email'><a href="mailto:amarx@glipple.com">Adam Marx - Chief Executive Officer</a></p>
-<p class='email'><a href="mailto:myles@glipple.com">Myles Shannon - Chief Technology Officer</a></p>
-<p class='email'><a href="mailto:mshem@glipple.com">Michael Shemesh - Chief Information Officer</a></p>
-<p class='email'><a href="mailto:smarx@glipple.com">Shelley Marx - Chief Content Officer</a></p>
+<p class='email'><a href="mailto:amarx@glipple.com">Adam Marx</a> - Chief Executive Officer</p>
+<p class='email'><a href="mailto:myles@glipple.com">Myles Shannon</a> - Chief Technology Officer</p>
+<p class='email'><a href="mailto:mshem@glipple.com">Michael Shemesh</a> - Chief Information Officer</p>
+<p class='email'><a href="mailto:smarx@glipple.com">Shelley Marx</a> - Chief Content Officer</p>
 
 <br />
 <p>Send us an email!</p>
