8bc7ef296782e0c3ea7c330232c62a814c737d19
Myles Shannon <mylios101@gmail.com>
2015-04-13 23:42:40 -0400

    add blog post

--- views/blog.php
+++ views/blog.php
@@ -1,5 +1,13 @@
-<h1>Blog</h1>
+<h1>Music Is Freedom</h1>
 
-<p>
-	
-</p>
+<p>Welcome! Glipple is the culmination of years I’ve spent in the music industry, as an artist, a journalist, a DJ, and now, an entrepreneur. The most exciting thing about Glipple for me is that it’s exactly the kind of thing I would have killed for during my blogging or DJ days; a site that’s full of amazing, independent artists from around the world, where access to music is limited only by your imagination and energy level!</p>
+<p>No more walled-garden dynamics to act as barriers for these incredible artists and the rabid fans who love them; we wanted to create something that was so pure—so much about freedom and the love of music—that we decided that anyone with an internet connection was welcome. No usernames or passwords required, and free access to streaming, downloading, and radio functionality—and all of it completely legal!</p>
+<p>Though we’re currently in beta (I promise things will get a lot prettier in the near future!), we’re working hard on an interesting algorithm that will set us far apart from what people are used to dealing with when they use music services. Suffice it to say that the old, “only 6 skips per month,” playlist-oriented radio function doesn’t do it for us; we’re working on something that will give artists and their fans more power than they’ve ever had before. The more interaction an artist drives to their music through their own marketing—on Facebook, Twitter, YouTube—the more their music will be rotated in the radio stream. It’s a real radio dynamic, and one that artists and their fans can effectively control. We’re building the steering wheel and taking our hands off—you drive.</p>
+<p>The digital age has been scary for the music industry thus far, but it need not be any longer. While other services are busy trying to conform to an obsolete business model that undercuts artists and frustrates all of those involved, we decided to do something different. We got rid of royalties altogether; they never amounted to anything in anyone’s pocket anyway (for independent artists especially), and only cause headaches as an obsolete method of business. We’re filling the void with a power dynamic that lies with the artists and with their fans; something that independent artists can realistically use as a tool.</p>
+<p>This is  for all the music addicts out there who can never get enough; all the people—the bloggers, the college DJs, the garage bands, the music lovers—who have a visceral need to know the next great artist on the horizon. Here’s your free access.</p>
+<p>And to those artists out there dying for a chance to find a fanbase: here’s your chance. You now have more control than you’ve ever had—use it.</p>
+<p>And that’s it. It’s as simple as that. Music is freedom, and now it’s free.</p>
+
+<h2>Keep rockin!</h2>
+
+<p>					—Adam Marx,  Cofounder & CEO</p>
